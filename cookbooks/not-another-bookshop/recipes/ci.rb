66623074028842052ae60d90cd566560e56e5fe3
Jon Burgess <jkburges@gmail.com>
2013-07-26 17:17:59 +1000

    Mostly working now, although it needs some refactoring.

--- cookbooks/not-another-bookshop/recipes/ci.rb
+++ cookbooks/not-another-bookshop/recipes/ci.rb
@@ -1,36 +1,44 @@
 #
 # Continuous integration config for our app.
 #
-include_recipe 'jenkins::server'
 
-# We need the git and grails plugins.
-# TODO: check for already installed plugins, to avoid unecessary restarts.
+# TODO: check jenkins is ready at the beginning
+# TODO: only install plugin that aren't already installed
+
 %w{ git grails }.each do |plugin|
-  jenkins_cli "install-plugin #{plugin}" do
-    notifies :run, "jenkins_cli[safe-restart]", :delayed
+  jenkins_cli "install-plugin #{plugin}"   # TODO: sometimes this times out - why?
+  jenkins_cli 'safe-restart'
+
+  # Make sure the server has properly restarted (from above plugin installation).
+  # TODO: cut and paste from https://github.com/opscode-cookbooks/jenkins/blob/master/recipes/server.rb
+  # Is there a way to re-use the code from there?  (Cannot simply include_recipe "jenkins::server", as
+  # that results in the jenkins server ending up on the app VM).
+  # TODO: refactor
+  ruby_block "block_until_api_operational" do
+
+    block do
+      test_url = URI.parse("#{node['jenkins']['server']['url']}/api/json")
+      Chef::Log.info "Waiting until the Jenkins API is responding (url: #{test_url})"
+      until JenkinsHelper.endpoint_responding?(test_url) do
+        sleep 1
+        Chef::Log.debug(".")
+      end
+    end
   end
 end
 
-jenkins_cli 'safe-restart' do
-  action :nothing
-  notifies :create, "ruby_block[block_until_operational]", :immediately  # wait for restart.
-end
 
+job_name = 'not-another-bookshop'
+job_config = File.join('/tmp', "#{job_name}-jenkins-config.xml")
 
 # Create a job for our app.
-job_name = 'not-another-bookshop'
-
-job_config = File.join('/tmp', "#{job_name}-jenkins-config.xml")
+cookbook_file job_config do
+  source "#{job_name}-jenkins-config.xml"
+  notifies :update, "jenkins_job[#{job_name}]", :immediately
+  notifies :build, "jenkins_job[#{job_name}]", :immediately
+end
 
 jenkins_job job_name do
   action :nothing
   config job_config
-end
-
-cookbook_file job_config do
-  source "#{job_name}-jenkins-config.xml"
-  action :nothing
-  subscribes :create, "ruby_block[block_until_operational]", :delayed
-  notifies :update, resources(:jenkins_job => job_name), :immediately
-  notifies :build, resources(:jenkins_job => job_name), :immediately
 end
