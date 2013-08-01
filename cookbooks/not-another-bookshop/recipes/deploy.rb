ee7630eb4bf96de2732a52437e63774bdae2c86f
Jon Burgess <jkburges@gmail.com>
2013-08-01 21:09:43 +1000

    Tomcat now starts without any exceptions.

--- cookbooks/not-another-bookshop/recipes/deploy.rb
+++ cookbooks/not-another-bookshop/recipes/deploy.rb
@@ -7,17 +7,22 @@ application 'not-another-bookshop' do
   repository "#{node['jenkins']['server']['url']}/job/not-another-bookshop/lastSuccessfulBuild/artifact/target/not-another-bookshop-0.1.war"
   scm_provider Chef::Provider::RemoteFile::Deploy
 
-  java_webapp
+  java_webapp do
+    context_template "context.xml.erb"
+  end
+
   tomcat
 
+  before_restart do
+    directory node['tomcat']['base'] do
+      owner node['tomcat']['user']
+      group node['tomcat']['group']
+    end
+
+    # Remove ROOT.xml or else we get exceptions in the log.
+    file "#{node['tomcat']['config_dir']}/Catalina/localhost/ROOT.xml" do
+      action :delete
+    end
+  end
 end
 
-directory node['tomcat']['base'] do
-  owner node['tomcat']['user']
-  group node['tomcat']['group']
-end
-
-# Remove ROOT.xml or else we get exceptions in the log.
-file "#{node['tomcat']['config_dir']}/Catalina/localhost/ROOT.xml" do
-  action :delete
-end
